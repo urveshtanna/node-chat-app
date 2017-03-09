f0f7152d9bd27984e381e1b2803d47ba78304ee9
Urvesh Tanna <urvesh.tanna@truce.in>
2017-03-09 10:56:49 +0530

    added config file for saving constants

---
+++ Config.js
@@ -0,0 +1,10 @@
+var ADMIN_NAME = 'Admin';
+var WELCOME_MESSAGE = 'Welcome to the chat app';
+var NEW_USER_MESSAGE = 'New User is added to the app';
+
+
+module.exports = {
+  ADMIN_NAME,
+  WELCOME_MESSAGE,
+  NEW_USER_MESSAGE
+};
--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ const express = require('express');
 const port = process.env.PORT || 3000;
 const http = require('http');
 const socket = require('socket.io');
+const config = require(path.join(__dirname,'../Config'));
 
 console.log(PUBLIC_PATH);
 
@@ -14,7 +15,22 @@ var io = socket(server);
 io.on('connection',(socket)=>{
   console.log('New User is now connect ');
 
-//to listen on this event sent from index.js ie from web page to server
+
+  //socket.emit from admin text welcomes new user app
+  socket.emit('newMessage',{
+    from : config.ADMIN_NAME,
+    body : config.WELCOME_MESSAGE,
+    createAt : new Date().getTime()
+  });
+
+  //socket.broadcast.emit to exist one saying new user is Connected
+  socket.broadcast.emit('newMessage',{
+    from : config.ADMIN_NAME,
+    body : config.NEW_USER_MESSAGE,
+    createAt : new Date().getTime()
+  });
+
+  //to listen on this event sent from index.js ie from web page to server
   socket.on('createMessage',(newMessage)=>{
     console.log('created new message',newMessage);
     io.emit('newMessage',{
@@ -22,6 +38,13 @@ io.on('connection',(socket)=>{
       body: newMessage.body,
       createAt : new Date().getTime()
     });
+
+    //To send message to other user not the current user
+    // socket.broadcast.emit('newMessage',{
+    //   from : newMessage.from,
+    //   body : newMessage.body,
+    //   createAt : new Date().getTime()
+    // });
   });
 
   socket.on('disconnect',()=>{
